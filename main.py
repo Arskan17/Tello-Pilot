e9eee3a73422a526b7e3434737e2c7ed492ce027
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-07-16 18:03:27 +0000

    Screen set-up

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            run = False
+
+        if event.type == pygame.KEYDOWN:
+            print("Key pressed")
+
+        if event.type == pygame.KEYUP:
+            print("Key released")
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("Mouse clicked")
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            print("Mouse released")
+
+        if event.type == pygame.MOUSEMOTION:
+            print("Mouse moving")
+
+        if event.type == pygame.MOUSEWHEEL:
+            print("Scroll-wheel moving/clicked")
+
+pygame.quit()
